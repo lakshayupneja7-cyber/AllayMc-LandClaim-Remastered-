ef61bf63bfb8f501e8cfcaed2fb6b3b7ec9877ce
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-20 04:47:57 +0530

    Update ClaimProvider.java

--- src/main/java/com/allaymc/landclaimremastered/hooks/ClaimProvider.java
+++ src/main/java/com/allaymc/landclaimremastered/hooks/ClaimProvider.java
@@ -1,4 +1,4 @@
-package com.allaymc.landclaimremastered.hooks;
+package com.allaymc.landclaimremastered.hook;
 
 import com.allaymc.landclaimremastered.model.ClaimContext;
 import org.bukkit.Location;
@@ -7,12 +7,8 @@ import org.bukkit.entity.Player;
 import java.util.Optional;
 
 public interface ClaimProvider {
-
     String getName();
-
     boolean isAvailable();
-
     Optional<ClaimContext> getClaimAt(Location location);
-
     int getTotalClaimBlocks(Player player);
 }
