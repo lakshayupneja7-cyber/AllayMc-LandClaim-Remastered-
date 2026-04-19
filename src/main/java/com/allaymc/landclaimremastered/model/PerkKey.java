ee2a72e73a31c9d2ae0be6313ead08e881afb3da
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-20 01:26:41 +0530

    Create PerkKey.java

---
+++ src/main/java/com/allaymc/landclaimremastered/model/PerkKey.java
@@ -0,0 +1,23 @@
+package com.allaymc.landclaimremastered.model;
+
+public enum PerkKey {
+    VERDANT_PULSE,
+    IRON_RHYTHM,
+    SKYBOUND,
+
+    TRAILBLAZER,
+    STONEHEART,
+    DEEP_FOCUS,
+
+    WINDSTEP,
+    HEARTHWARMTH,
+    MOONSIGHT,
+
+    FEATHERFALL_WARD,
+    BUILDERS_GRACE,
+    HEARTHLIGHT,
+
+    STORMSTRIDE,
+    TITAN_BLOOD,
+    EVERGLOW
+}
