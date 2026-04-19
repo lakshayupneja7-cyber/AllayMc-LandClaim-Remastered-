b99ee1538777ebbb8e2aed19fad895327793736d
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-20 01:25:03 +0530

    Create ConfigKeys.java

---
+++ src/main/java/com/allaymc/landclaimremastered/config/ConfigKeys.java
@@ -0,0 +1,20 @@
+package com.allaymc.landclaimremastered.config;
+
+public final class ConfigKeys {
+
+    private ConfigKeys() {
+    }
+
+    public static final String DATABASE_TYPE = "database.type";
+    public static final String DATABASE_SQLITE_FILE = "database.sqlite-file";
+
+    public static final String CLAIMS_PROVIDER = "claims.provider";
+
+    public static final String PERKS_SWITCH_COOLDOWN_SECONDS = "perks.switch-cooldown-seconds";
+    public static final String PERKS_APPLY_REFRESH_TICKS = "perks.apply-refresh-ticks";
+    public static final String PERKS_TRUSTED_DEFAULT_MODE = "perks.trusted-default-mode";
+
+    public static final String UI_SCOREBOARD_ENABLED = "ui.scoreboard.enabled";
+    public static final String UI_ACTIONBAR_ENTER_MESSAGE = "ui.actionbar-enter-message";
+    public static final String UI_SHOW_COMING_SOON_TIERS = "ui.show-coming-soon-tiers";
+}
