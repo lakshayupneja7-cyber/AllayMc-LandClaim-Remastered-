1d440612b2630ca6df92244dfae070ebd11e6b14
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-20 01:27:00 +0530

    Create PerkDefinition.java

---
+++ src/main/java/com/allaymc/landclaimremastered/model/PerkDefinition.java
@@ -0,0 +1,13 @@
+package com.allaymc.landclaimremastered.model;
+
+import org.bukkit.Material;
+
+public record PerkDefinition(
+        PerkKey key,
+        String displayName,
+        String description,
+        Tier unlockTier,
+        Material icon,
+        boolean comingSoon
+) {
+}
