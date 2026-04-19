ca703c0f484bfc51cff851ece8ca5ec4c3944a68
lakshayupneja7-cyber <lakshayupneja7@gmail.com>
2026-04-20 01:32:47 +0530

    Create CommandMessages.java

---
+++ src/main/java/com/allaymc/landclaimremastered/commands/CommandMessages.java
@@ -0,0 +1,12 @@
+package com.allaymc.landclaimremastered.commands;
+
+public final class CommandMessages {
+
+    private CommandMessages() {
+    }
+
+    public static final String PLAYERS_ONLY = "players-only";
+    public static final String NO_PERMISSION = "no-permission";
+    public static final String NO_CLAIM_HERE = "no-claim-here";
+    public static final String RELOADED = "reloaded";
+}
